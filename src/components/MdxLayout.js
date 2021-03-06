5b15217d826b2c6c2e44d7767f92cdefc149c3a3
Ajay <ajay.wu@take2.health>
2021-03-06 18:12:56 +0800

    feat: add MDXProvider demo

--- src/components/MdxLayout.js
+++ src/components/MdxLayout.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { MDXProvider } from '@mdx-js/react';
+
+const MyH1 = (props) => <h1 style={{ color: 'tomato', background: 'red' }} {...props} />;
+const MyParagraph = (props) => (
+  <h2 style={{ fontSize: '18px', lineHeight: 1.6, backgroundColor: 'blue', textAlign: 'center' }} {...props} />
+);
+
+const components = {
+  h1: MyH1,
+  h2: MyParagraph,
+};
 
 const MdxLayout = ({ children, pageContext }) => (
   <div>
     <h1>My Layout</h1>
     <div>{pageContext.frontmatter.date}</div>
-    <div style={{ width: 400, margin: '0 auto', background: 'blue' }}>{children}</div>
+    <div style={{ width: 400, margin: '0 auto', background: 'blue' }}>
+      <MDXProvider components={components}>{children}</MDXProvider>
+    </div>
   </div>
 );
 
--- src/templates/articleTemplate.js
+++ src/templates/articleTemplate.js
@@ -1,16 +1,5 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { MDXProvider } from '@mdx-js/react';
-
-const MyH1 = (props) => <h1 style={{ color: 'tomato' }} {...props} />;
-const MyParagraph = (props) => (
-  <h2 style={{ fontSize: '18px', lineHeight: 1.6, backgroundColor: 'blue', textAlign: 'center' }} {...props} />
-);
-
-const components = {
-  h1: MyH1,
-  h2: MyParagraph,
-};
 
 export default function Template({ data }) {
   const { mdx } = data; // data.markdownRemark holds your post data
@@ -20,9 +9,7 @@ export default function Template({ data }) {
       <div className='blog-post'>
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
-        <MDXProvider components={components}>
-          <div className='blog-post-content' dangerouslySetInnerHTML={{ __html: html }} />
-        </MDXProvider>
+        <div className='blog-post-content' dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </div>
   );
