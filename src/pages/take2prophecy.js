e7a2f7bde1dcafac19330206622ba8b7a17c21a5
Ajay <ajay.wu@take2.health>
2021-03-09 16:44:06 +0800

    feat: add take2prophecy page

---
+++ src/pages/take2prophecy.js
@@ -0,0 +1,62 @@
+/** @jsx jsx */
+import React, { useState, useEffect } from 'react';
+import { jsx } from 'theme-ui';
+import { withPrefix } from 'gatsby';
+import scrollTo from 'gatsby-plugin-smoothscroll';
+import styles from './take2introduction.module.css';
+import HoverBox from '/src/components/HoverBox/HoverBox';
+import MenusData from '/src/data/menu.json';
+const introductionPage = ({ location }) => {
+  const [curMenu, setCurMenu] = useState([]);
+  useEffect(() => {
+    setCurMenu(MenusData.find((menu) => menu.path === withPrefix(location.pathname))?.children || []);
+  }, [location.pathname]);
+  return (
+    <div className={styles.wrapper} sx={{ backgroundImage: 'gradientMuted' }}>
+      <div
+        sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', py: 6, mx: 'auto', maxWidth: 'maxWidth' }}
+      >
+        {curMenu.map((child) => (
+          <HoverBox
+            key={child.path}
+            sxx={{
+              height: '50px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontSize: 2,
+              fontWeight: 'bold',
+              flexGrow: 1,
+              minWidth: '300px',
+              mx: 4,
+              mb: 6,
+              boxShadow: '0 25px 25px 0 rgba(0, 0, 0, 0.1)',
+            }}
+            onClick={() => {
+              scrollTo(`#${child.id}`);
+            }}
+          >
+            {child.name}
+          </HoverBox>
+        ))}
+      </div>
+      <section id={curMenu[0]?.id} sx={{ mx: 'auto', maxWidth: 'maxWidth', height: '100vh' }}>
+        test
+      </section>
+      <section id={curMenu[1]?.id} sx={{ mx: 'auto', maxWidth: 'maxWidth', height: '100vh' }}>
+        test
+      </section>
+      <section id={curMenu[2]?.id} sx={{ mx: 'auto', maxWidth: 'maxWidth', height: '100vh' }}>
+        test
+      </section>
+      <section id={curMenu[3]?.id} sx={{ mx: 'auto', maxWidth: 'maxWidth', height: '100vh' }}>
+        test
+      </section>
+      <section id={curMenu[4]?.id} sx={{ mx: 'auto', maxWidth: 'maxWidth', height: '100vh' }}>
+        test
+      </section>
+    </div>
+  );
+};
+
+export default introductionPage;
