794ab1467a2d2c1341b1ef83677e85510cbf50e4
Ajay <ajay.wu@take2.health>
2021-03-22 16:11:59 +0800

    fix: images src

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
     ],
   },
   pathPrefix: '/github-pages-demo',
-  assetPrefix: `https://mask2.github.io/github-pages-demo`,
+  assetPrefix: `https://mask2.github.io`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
