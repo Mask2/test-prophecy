d04b03fe7dd0452f4fb3c844d99c0795715b790f
crossz <ukcrossa@gmail.com>
2021-08-03 17:20:48 +0800

    :tada: BasePath in Gatsby.js.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,46 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: 'https://www.yourdomain.tld',
-    title: 'campaign-test',
-  },
-  pathPrefix: '/test-prophecy',
-  assetPrefix: `https://mask2.github.io`,
-  plugins: [
-    'gatsby-plugin-image',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        icon: 'src/images/download.png',
-      },
+    siteMetadata: {
+        siteUrl: 'https://www.yourdomain.tld',
+        title: 'campaign-test',
     },
-    {
-      resolve: `gatsby-plugin-material-ui`,
-      options: {
-        disableAutoprefixing: false,
-        disableMinification: false,
-      },
-    },
+    pathPrefix: `/campaign`,
+    // assetPrefix: `https://mask2.github.io`,
+    plugins: [
+        'gatsby-plugin-image',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sitemap',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/download.png',
+            },
+        },
+        {
+            resolve: `gatsby-plugin-material-ui`,
+            options: {
+                disableAutoprefixing: false,
+                disableMinification: false,
+            },
+        },
 
-    'gatsby-plugin-mdx',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'pages',
-        path: './src/pages/',
-      },
-      __key: 'pages',
-    },
-  ],
+        'gatsby-plugin-mdx',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+    ],
 }
