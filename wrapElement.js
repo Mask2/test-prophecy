1cfb1eb3bc2e620f8b8ffd67b07a21f4bb3619db
Ajay <ajay.wu@take2.health>
2021-08-11 20:14:47 +0800

    :rocket: Fix build errors

--- wrapElement.js
+++ wrapElement.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
 import getTheme from './src/themes'
-import Layout from './src/components/layout'
+import Layout from './src/components/Layout'
 
 // Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
 const wrapPageElement = ({ element, props }) => (
