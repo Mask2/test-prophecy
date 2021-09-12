8d75c329e69255fbb89b394f525794dfa80d5066
Ajay <ajay.wu@take2.health>
2021-09-12 22:52:47 +0800

    :bug: Fix tracking code

--- src/html.js
+++ src/html.js
@@ -73,34 +73,43 @@ export default function HTML(props) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            !(function (f, b, e, v, n, t, s) {
-              if (f.fbq) return
-              n = f.fbq = function () {
-                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-              }
-          
-              if (!f._fbq) f._fbq = n
-              n.push = n
-              n.loaded = !0
-              n.version = '2.0'
-          
-              n.queue = []
-              t = b.createElement(e)
-              t.async = !0
-          
-              t.src = v
-              s = b.getElementsByTagName(e)[0]
-          
-              s.parentNode.insertBefore(t, s)
-            })(
-              window,
-              document,
-              'script',
-          
-              'https://connect.facebook.net/en_US/fbevents.js'
-            )
-            fbq('init', '437103407310894')
-            // fbq('track', 'PageView')
+            !function(f,b,e,v,n,t,s)
+ 
+ 
+ 
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            
+            
+            
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            
+            
+            
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            
+            
+            
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            
+            
+            
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            
+            
+            
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            
+            
+            
+            'https://connect.facebook.net/en_US/fbevents.js');
+            
+            
+            
+            fbq('init', '437103407310894');
+            
+            
+            
+            fbq('track', 'PageView');           
               `,
           }}
         />
