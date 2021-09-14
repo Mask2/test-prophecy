dc84c7cf3efa6d1faebf2935e7c9618aaeda5cb9
Ajay <ajay.wu@take2.health>
2021-09-14 17:50:57 +0800

    :rocket: Test tracking code

--- src/html.js
+++ src/html.js
@@ -70,7 +70,7 @@ export default function HTML(props) {
           }}
         />
         {/* Facebook Pixel Code */}
-        <script
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `
             !function(f,b,e,v,n,t,s)
@@ -109,10 +109,10 @@ export default function HTML(props) {
             
             
             
-            // fbq('track', 'PageView');           
+            fbq('track', 'PageView');           
               `,
           }}
-        />
+        /> */}
         <noscript>
           <img
             height='1'
